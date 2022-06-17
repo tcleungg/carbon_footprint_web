62112e23930b4f3b057c5b4bb4337dacb0014363
梁Eileen 梓晴 <eileen.leung@sourcezones.com>
2022-06-17 14:58:04 +0800

    create category pages

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home, name = "home"),
     path('index/', views.index, name = "index"),
     path('search/', views.search, name = "search"),
-    path('product/<pcces_encode>', views.product, name = "product" )
+    path('product/<pcces_encode>', views.product, name = "product"),
+    path('category/', views.category, name = "category"),
+    path('category/<primary_category>', views.primary_category_products, name = "primary_category_products"),
+    path('category/<primary_category>/<secondary_category>', views.secondary_category_products, name = "secondary_category_products"),
 ]
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 import logging
 
-from . models import Product 
+from . models import Product, PrimaryCategory, SecondaryCategory, \
+                    AnalyticMethod, DataLevel, Co2Allocation, PccesEncode
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
@@ -16,10 +17,43 @@ def search(request):
     return render(request, 'pages/search_result.html', 
                         {"query":query, "products":products})
 
+def slice_pcces_encode(pcces_encode):
+    return [pcces_encode[0], pcces_encode[1:6], pcces_encode[6],pcces_encode[7],pcces_encode[8],pcces_encode[9],pcces_encode[10]]
+
 def product(request, pcces_encode):
-    product = Product.objects.get(pcces_encode=pcces_encode)
+    product = Product.objects.select_related().get(pcces_encode=pcces_encode)
+    pcces_encode_desc = PccesEncode.objects.get(pcces_encode=pcces_encode)
+    pcces_encode = slice_pcces_encode(pcces_encode)
     if product.review_date == None: 
         product.review_date = ''
     product.create_date = product.create_date.strftime("%m/%d/%Y")
     return render(request, 'pages/product.html', 
-                        {"product":product})
+                            {"product":product, 
+                            "pcces_encode":pcces_encode, 
+                            "pcces_encode_desc":pcces_encode_desc})
+
+def category(request):
+    categories = PrimaryCategory.objects.all().prefetch_related('secondary_categories')
+    for category in categories:
+        category.category_id = category.category_id.strip()
+    return render(request, 'pages/category.html', {"categories":categories})
+
+def primary_category_products(request, primary_category):
+    try:
+        products = Product.objects.select_related('primary_category').filter(primary_category__name=primary_category)
+    except Product.DoesNotExist:
+        products = {}
+    return render(request, 'pages/category_products.html', 
+                            {"primary_category":primary_category, "products":products})
+
+def secondary_category_products(request, primary_category, secondary_category):
+    try:
+        products = Product.objects.select_related('primary_category') \
+                    .filter(primary_category__name=primary_category) \
+                    .select_related('secondary_category').filter(secondary_category__name=secondary_category)
+    except Product.DoesNotExist:
+        products = {}
+    return render(request, 'pages/category_products.html', 
+                            {"primary_category":primary_category,
+                                "secondary_category":secondary_category,
+                                "products":products})
